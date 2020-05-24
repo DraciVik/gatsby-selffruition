eadd70b123e386b14dc387e86ded63a04871afd5
DraciVik <viktor.stojanov90@gmail.com>
2020-05-24 03:31:46 +0200

    Added initial configurations

--- src/gatsby-theme-sky-lite/style/theme.js
+++ src/gatsby-theme-sky-lite/style/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
 	palette: {
 		primary: {
-			main: "#ccc",
+			main: "#6902bf",
 		},
 		secondary: {
 			main: "#6902bf",
